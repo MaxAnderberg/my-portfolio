d74c5cf9370519aa26946c20f7a7a1d9779708fe
Max Anderberg <maxanderberg@gmail.com>
2021-09-01 15:32:44 +0200

    added graphql query for getting first version of data from the CMS and a rudamentary project component to display description

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project = ( {description} ) => {
+  console.log(description)
+  return (
+    <div>
+      {description}
+    </div>
+  )
+}
+
+export default Project;
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,43 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
+import Project from '../components/Project'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Portfolio = () => (
-  <Layout>
-    <Seo title="Portfolio" />
-    <h1>Hi from the Portfolio page</h1>
-    <p>Here all my projects will be displayed through a headless CMS</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+
+const Portfolio = ({data}) => {
+  const nodes = data.allContentfulProject.edges
+  return (
+    <Layout>
+      <Seo title="Portfolio" />
+      <h1>Hi from the Portfolio page</h1>
+      <p>Here all my projects will be displayed through a headless CMS</p>
+      {nodes.map(item => <Project description={item.node.description.description}/>)}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql `
+  query PortfolioPageQuery {
+    allContentfulProject {
+    edges {
+      node {
+        id
+        image {
+          id
+          fluid {
+            src
+          }
+        }
+        slug
+        description {
+          description
+        }
+      }
+    }
+  }
+}
+`
 
 export default Portfolio
