006d45bdaebb3dec8eca67db0d973a35ad58248a
Max Anderberg <maxanderberg@gmail.com>
2021-09-02 14:28:58 +0200

    added active link style

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -5,9 +5,9 @@ import './NavLinks.scss';
 const NavLinks = () => {
   return (
     <div className='navlinks-container'>
-      <Link to="/" activeStyle className='navlink'>Home</Link>
-      <Link to="/about" activeStyle className='navlink'>About</Link>
-      <Link to="/portfolio" activeStyle className='navlink'>Portfolio</Link> 
+      <Link to="/" activeClassName='active' className='navlink'>Home</Link>
+      <Link to="/about" activeClassName='active' className='navlink'>About</Link>
+      <Link to="/portfolio" activeClassName='active' className='navlink'>Portfolio</Link> 
     </div>
   )
 }
