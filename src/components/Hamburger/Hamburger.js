2f9d864fc4049d42247223633ba722f83a4975dc
Max Anderberg <maxanderberg@gmail.com>
2021-09-02 11:09:47 +0200

    removed old imports

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './hamburger.scss';
 
 const Hamburger = ({ isOpen, setOpen}) => {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.scss"
 
