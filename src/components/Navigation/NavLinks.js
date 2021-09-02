3a38122d4da27fddd8d78d6e87e25cf2de22e01f
Max Anderberg <maxanderberg@gmail.com>
2021-09-02 10:59:55 +0200

    positioned the navlinks within the header

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from "gatsby"
+import { Link } from "gatsby";
+import './NavLinks.css';
 
 const NavLinks = () => {
   return (
-    <div>
-      <Link to="/" activeStyle>Home</Link>
-      <Link to="/about" activeStyle>About</Link>
-      <Link to="/portfolio" activeStyle>Portfolio</Link> 
+    <div className='navlinks-container'>
+      <Link to="/" activeStyle className='navlink'>Home</Link>
+      <Link to="/about" activeStyle className='navlink'>About</Link>
+      <Link to="/portfolio" activeStyle className='navlink'>Portfolio</Link> 
     </div>
   )
 }
