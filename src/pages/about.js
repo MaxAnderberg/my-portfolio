cc7e449fdf9564b2243f659ed70c743a548e8681
Max Anderberg <maxanderberg@gmail.com>
2021-09-04 20:28:55 +0200

    General layout of the about page complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,35 @@
 import * as React from "react"
+import './styles/about.scss';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image";
 
 const About = () => (
   <Layout>
     <Seo title="About" />
-    <h1>Hi from the About page</h1>
-    <p>Hi I'm Max</p>
+    <div className='about-container'>
+      <h1>Hello there!</h1>
+      <p> My journey starts In 2014 when I started the bachelor programme Computational linguistics at Uppsala University. I started programming in python, and during my last year I pursued more programming courses like: 
+			  Java, Database design, interface programming and a theory course called Human Computer Interactions. 
+        I got my first job in September 2017 at Baltic Spot as a computational Linguistic, and they were a software company in the maritime business.
+      </p>
+      <section>
+        <h2>Skills</h2>
+        <section>
+          <h2>Frontend</h2>
+          <StaticImage src='../images/javascript.png' className='image'/>
+        </section>
+        <section>
+          <h2>Backend</h2>
+        </section>
+        <section>
+          <h2>Tools</h2>
+        </section>
+      </section>
+
+    </div>
+
   </Layout>
 )
 
