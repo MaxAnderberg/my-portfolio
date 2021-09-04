5e054bc49d0b8892f8d5d18c345b9cfe7ec39a61
Max Anderberg <maxanderberg@gmail.com>
2021-09-04 11:16:34 +0200

    completely reworked the design, added slide show, icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,57 @@
-import * as React from "react"
+import React, { useRef, useState, useEffect } from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import './styles/index.scss';
 
-const IndexPage = () => (
+const IndexPage = () => {
+  
+  const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+  const images = [
+    "https://photos.google.com/search/_tra_/photo/AF1QipNT-irFVwOiIdffUqI2avCnMk29gPtcOcfiO0dO",
+    "https://photos.google.com/search/_tra_/photo/AF1QipN01Ud8d2vl2HfsD632WGQ9PSHHR6xRmbJ99y1P"
+   , "https://photos.google.com/search/_tra_/photo/AF1QipN01Ud8d2vl2HfsD632WGQ9PSHHR6xRmbJ99y1P"
+  ];
+  const [index, setIndex] = React.useState(0);
+  const delay = 2500;
+
+  React.useEffect(() => {
+    setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+    return () => {};
+  }, [index]);
+
+  return (
   <Layout>
-    <img className='testImg zoom-in-zoom-out' src="https://lh3.google.com/pw/AM-JKLVjBjAemEgM4_Cw4oQNc8a5zSDEHGT6-2H9zY5UL-FI8gS8E8sbSfKMg0uXXC-DiIvY1VF0KLMoHSseVxO-zZdmRKzGXKk=w1390-h1852-no?authuser=0"></img>
-
+    <div className="homepage-container">
       <div className='bio-intro'>
-        <h3 className='bio-intro__greeting'>Hi, I'm</h3>
-        <h2 className='bio-intro__name'>Max Anderberg</h2>
-        <h4 className='bio-intro__title'>Fullstack Developer</h4>
-        <div className='bio-intro__icons-container'>
-          <a href="https://github.com/MaxAnderberg"><StaticImage className='icon' src='../icons/github-128.png'/></a>
-          <a href="https://linkedin.com/in/maxanderberg"><StaticImage className='icon' src='../icons/linkedin-128.png'/></a>
+          <h3 className='bio-intro__greeting'>Hi, I'm</h3>
+          <h2 className='bio-intro__name'>Max Anderberg</h2>
+          <h4 className='bio-intro__title'>Fullstack Developer</h4>
         </div>
-      </div>
+        <div className="item-c">
+        <div className="slideshow ">
+          <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+            <StaticImage className='slide' src="../images/javascript.png" alt=""/>
+            <StaticImage className='slide' src="../images/html5.png" alt=""/>
+            <StaticImage className='slide' src="../images/css3.png" alt=""/>
+          </div>
+        </div>
+        </div>
 
-    <div className='diagonal-box'></div>
+        <div className='bio-intro__icons-container item-d'>
+            <a href="https://github.com/MaxAnderberg"><StaticImage className='icon' src='../icons/github-128.png'/></a>
+            <a href="https://linkedin.com/in/maxanderberg"><StaticImage className='icon' src='../icons/linkedin-128.png'/></a>
+          </div>
+    </div>
     <Seo title="Home" />
   </Layout>
-
 )
+}
 
 export default IndexPage
