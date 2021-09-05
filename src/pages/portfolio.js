6c77984bf96f7451f35bd3a6255e4542a40b589e
Max Anderberg <maxanderberg@gmail.com>
2021-09-05 11:53:53 +0200

    Fixed project structure

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import Project from '../components/Project'
+import Project from '../components/Project/Project'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import './styles/portfolio.scss';
