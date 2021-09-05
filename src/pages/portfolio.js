655d9429101f357ceae89ba5956c962e364f8cb0
Max Anderberg <maxanderberg@gmail.com>
2021-09-05 11:38:13 +0200

    Set up portfolio sass

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,16 +3,18 @@ import { graphql, Link } from "gatsby"
 import Project from '../components/Project'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import './styles/portfolio.scss';
 
 const Portfolio = ({data}) => {
   const nodes = data.allContentfulProject.edges
   return (
     <Layout>
       <Seo title="Portfolio" />
-      <h1>Hi from the Portfolio page</h1>
-      <p>Here all my projects will be displayed through a headless CMS</p>
-      {nodes.map(item => <Project description={item.node.description.description} image={item.node.image.fluid.src} title={item.node.title}/>)}
-      <Link to="/">Go back to the homepage</Link>
+      <div className='portfolio-container'>
+        <h1>Hi from the Portfolio page</h1>
+        <p>Here all my projects will be displayed through a headless CMS</p>
+        {nodes.map(item => <Project description={item.node.description.description} image={item.node.image.fluid.src} title={item.node.title}/>)}  
+      </div>
     </Layout>
   )
 }
