db1fa23b84bbdb4edf9b17082af3babcbd49d4a7
Max Anderberg <maxanderberg@gmail.com>
2021-09-05 19:19:24 +0200

    started styling cards

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import './Project.scss';
 
 const Project = ( props ) => {
   const {description, image, title, tags, githubLink, deploymentLink} = props;
+  const parsedTags = tags.split(' ');
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={image}/>
-      <p>{description}</p>
-      <p>{tags}</p>
-      <p>{githubLink}</p>
-      <p>{deploymentLink}</p>
-    </div>
+    <article className='card'>
+      <section className='card__title'>
+        <h1>{title}</h1>
+      </section>
+      <section className='card__info-container'>
+        <img className='card__image' src={image}/>
+        <p className='card__description'>{description}</p>
+      </section>
+      <section className='card__tags'>
+        <p>{parsedTags.map(item => (<span className='tags'>{item}</span>))}</p>
+      </section>
+      <section className='link-container'>
+        <a className='link__project' href={deploymentLink} >Live Code</a>
+        <a className='link__project' href={githubLink} >GitHub</a>
+      </section>
+    </article>
   )
 }
 
