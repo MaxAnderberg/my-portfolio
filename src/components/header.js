ce0d173498943fbaa6a13780f81c1b29c2f3c313
Max Anderberg <maxanderberg@gmail.com>
2021-09-06 15:50:10 +0200

    removed unused components

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,4 @@
 import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
 import Navbar from './Navigation/Navbar';
 import './Header.scss';
 const Header = () => (
