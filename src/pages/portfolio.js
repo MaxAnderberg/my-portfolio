424ef6e1aa429ed904de0128ed07380b09e51bb0
Max Anderberg <maxanderberg@gmail.com>
2021-09-10 16:19:11 +0200

    added another card alternative

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { graphql, Link } from "gatsby"
+import React, { useState } from 'react';
+import { graphql, Link, StaticImage } from "gatsby"
 import Project from '../components/Project/Project'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,12 +7,38 @@ import './styles/portfolio.scss';
 
 const Portfolio = ({ data }) => {
   const nodes = data.allContentfulProject.edges
+    
+  const [flip, setFlip] = useState(false);
 
+  const handleFlip = () => flip ? setFlip(false) : setFlip(true); 
+  
   return (
     <Layout>
       <Seo title="Portfolio" />
       <div className='portfolio-container'>
         <h1 className='portfolio__title'>Projects</h1>
+
+        <div className='kard'>
+          <div className={`kard__inner ${flip ? 'is-flipped' : ''}`} onClick={handleFlip}>
+            <div className='kard__face kard__face--front'>
+              <h2>Devolper Kard</h2>  
+            </div>   
+            <div className='kard__face kard__face--back'>
+                <div className='kard__content'>
+                  <div className='kard__header'>
+                    <img></img>
+                    <h2>Max Anderberg</h2>
+                  </div>
+                  <div className='kard__body'>
+                    <h3>Portfolio name</h3>
+                    <p>Lorem ipsum <strong>dolor</strong> sit amet, consectetur <strong>adipiscing</strong> elit. Sed id erat a magna lobortis dictum. Nunc est arcu, <strong>lacinia</strong> quis sapien placerat, <strong>laoreet</strong> tincidunt nulla.</p>
+                  </div>
+                  </div>
+                </div>
+              <div >
+            </div>            
+          </div>
+        </div>
         {nodes.map(item => 
           <Project 
             description={item.node.description.description} 
