50bc917773a830825b77a90cb209d6d16a54bbbb
Max Anderberg <maxanderberg@gmail.com>
2021-09-13 09:32:17 +0200

    styled link transitioning

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -4,11 +4,12 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import './NavLinks.scss';
 
 const NavLinks = () => {
+  const color = "#FFF";
   return (
     <div className='navlinks-container'>
-      <AniLink swipe direction="left" duration={0.5} to="/" activeClassName='active' className='navlink as'>Home</AniLink>
-      <AniLink swipe direction="left" duration={1} to="/about" activeClassName='active' className='navlink'>About</AniLink>
-      <AniLink swipe direction="left" duration={1} to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
+      <AniLink cover bg={color} direction="down" to="/" activeClassName='active' className='navlink as'>Home</AniLink>
+      <AniLink cover bg={color} direction="down" to="/about" activeClassName='active' className='navlink'>About</AniLink>
+      <AniLink cover bg={color} direction="down" to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
     </div>
   )
 }
