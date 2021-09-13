4cdf734e79491c936bd6981c151ddb6ef3ec3e16
Max Anderberg <maxanderberg@gmail.com>
2021-09-13 08:51:25 +0200

    added transition links

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from "gatsby";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import './NavLinks.scss';
 
 const NavLinks = () => {
   return (
     <div className='navlinks-container'>
-      <Link to="/" activeClassName='active' className='navlink as'>Home</Link>
-      <Link to="/about" activeClassName='active' className='navlink'>About</Link>
-      <Link to="/portfolio" activeClassName='active' className='navlink'>Portfolio</Link> 
+      <AniLink swipe direction="left" duration={0.5} to="/" activeClassName='active' className='navlink as'>Home</AniLink>
+      <AniLink swipe direction="left" duration={1} to="/about" activeClassName='active' className='navlink'>About</AniLink>
+      <AniLink swipe direction="left" duration={1} to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
     </div>
   )
 }
