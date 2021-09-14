d3a1e010a8a7628da07674b2c44e6cf182735914
Max Anderberg <maxanderberg@gmail.com>
2021-09-14 09:47:48 +0200

    set up basic architecture for a contact page

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -14,6 +14,7 @@ const NavLinks = () => {
       <AniLink cover direction="down" bg={coverProperties} duration={0.8} to="/" activeClassName='active' className='navlink as'>Home</AniLink>
       <AniLink paintDrip direction="down" bottom='exit' hex="#FE4880" duration={0.6} to="/about" activeClassName='active' className='navlink'>About</AniLink>
       <AniLink cover direction="down" bg={coverProperties} duration={0.6} to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
+      <AniLink cover direction="down" bg={coverProperties} duration={0.6} to="/contact" activeClassName='active' className='navlink'>Contact</AniLink> 
     </div>
   )
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Seo title="Contact" />
+    <h1>Contact here</h1>
+    <p>Welcome :)</p>
+  </Layout>
+)
+
+export default NotFoundPage
