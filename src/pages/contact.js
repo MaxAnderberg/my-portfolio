bf2c4331130bd603b1d75d724e3f3a7089c6c4a0
Max Anderberg <maxanderberg@gmail.com>
2021-09-14 11:34:45 +0200

    contact page with links set up

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,38 @@
 import * as React from "react"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import './styles/contact.scss';
+import { motion } from "framer-motion"
 
-const NotFoundPage = () => (
+
+const ContactPage = () => (
   <Layout>
     <Seo title="Contact" />
-    <h1>Contact here</h1>
-    <p>Welcome :)</p>
+      <section className='contact-container'>
+          <motion.h1 
+            transition={{duration:0.5, ease: "easeIn"}}
+            animate={{
+              scale: [0, 1]
+            }}
+            className='contact__title'>
+              maxanderberg@gmail.com
+            </motion.h1>
+          <ul className='contact__links'>
+            <motion.li
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <a className='contact__link' target="_blank" rel="noopener norefferer" href="https://linkedin.com/in/maxanderberg">Linkedin</a>
+            </motion.li>
+            <motion.li
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <a className='contact__link' target="_blank" href="https://github.com/MaxAnderberg/">Github</a>
+            </motion.li>
+          </ul>
+      </section>
   </Layout>
 )
 
-export default NotFoundPage
+export default ContactPage
