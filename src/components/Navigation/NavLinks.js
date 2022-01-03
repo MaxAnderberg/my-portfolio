1ee1699427c8fceb0068e69be48a1ff84cd9738f
Max Anderberg <maxanderberg@gmail.com>
2022-01-03 20:28:46 +0100

    removed unused component, added placeholder link for blog

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -15,6 +15,7 @@ const NavLinks = () => {
       <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/about" activeClassName='active' className='navlink'>About</AniLink>
       <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
       <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/contact" activeClassName='active' className='navlink'>Contact</AniLink> 
+      <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="" activeClassName='active' className='navlink'>Blog</AniLink> 
     </div>
   )
 }
