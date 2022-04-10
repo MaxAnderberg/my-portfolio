5fe4c0d9e3cc536d4c3c462657db412d3ae8e414
Max Anderberg <maxanderberg@gmail.com>
2022-04-10 19:08:11 +0200

    Removed AniLink (direction animation) from NavLinks.js

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Link } from "gatsby";
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 import './NavLinks.scss';
 
 const NavLinks = () => {
-  
-  const coverProperties = `
-    #212121
-`;
-
   return (
     <div className='navlinks-container'>
-      <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/" activeClassName='active' className='navlink as'>Home</AniLink>
-      <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/about" activeClassName='active' className='navlink'>About</AniLink>
-      <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/portfolio" activeClassName='active' className='navlink'>Portfolio</AniLink> 
-      <AniLink cover direction="left" bg={coverProperties} duration={0.7} to="/contact" activeClassName='active' className='navlink'>Contact</AniLink> 
+      <Link to="/" activeClassName='active' className='navlink as'>Home</Link>
+      <Link to="/about" activeClassName='active' className='navlink'>About</Link>
+      <Link to="/portfolio" activeClassName='active' className='navlink'>Portfolio</Link>
+      <Link to="/contact" activeClassName='active' className='navlink'>Contact</Link>
     </div>
   )
 }
