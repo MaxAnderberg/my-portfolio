5851710553708dd8a202ea7e4463b6e3c3d4c7b7
Max <maxanderberg@gmail.com>
2023-12-17 20:22:50 +0100

    Add dependencies and update main.go

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// main is the entry point of the program.
 func main() {
 	fmt.Println("Hello World")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
 
+func hello(c echo.Context) error {
+
+	return c.String(200, "Hello World")
 }
